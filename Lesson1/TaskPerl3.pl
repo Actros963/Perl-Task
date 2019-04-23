a52d4fc2a39213b7deff4e36a6560ed8e9672650
Александр Новожилов <mb934@yandex.ru>
2019-04-23 22:12:18 +0400

    TaskPerl3.pl complete

--- Lesson1/TaskPerl3.pl
+++ Lesson1/TaskPerl3.pl
@@ -3,14 +3,55 @@ use strict;
 use 5.018;
 use Data::Dumper;
 
-my $catalog = [];
-my $filter_key = '';
-my $sort_key = '';
+my $cat = [
+    {
+        number => 18,
+        model  => "A",
+        color  => "green",
+        weight => "50000",
+        },
+    {
+        number => 12,
+        model  => "B",
+        color  => "yellow",
+        weight => "20000",
+        },
+    {
+        number => 3,
+        model  => "C",
+        color  => "black",
+        weight => "24000",
+        },
+    {
+        number => 46,
+        model  => "D",
+        color  => "white",
+        weight => "44000",
+        },
+    {
+        number => 2,
+        model  => "E",
+        color  => "purple",
+        weight => "46000",
+        },    	
+    ];
+my $f_key = 'weight';
+my $s_key = 'number';
+sub fil{
+    my($cat, $f_key, $s_key) = @_;
+    my @res;
+    foreach my $el (@$cat){
+        if ($el->{$f_key} < 47000){
+            push @res, $el
+        }
+    };
+    print Dumper @res;
+    my @sort_arr = sort{
+        $a->{$s_key} <=> $b->{$s_key}
+	}@res;
+    print Dumper @sort_arr;
+};
 
-sub get_filter_{
-    my($catalog, $filter_key, $sort_key) = @_;
-    my @filter_cat = grep $filter_key, @$catalog;
-    my @sorted_cat = sort $sort_key, @filter_cat;
-    print Dumper @sorted_cat;
+fil($cat, $f_key, $s_key);
 };
 
