7b9f9152adccde3a5bb8cbec056804cb6ea0e44a
Александр Новожилов <mb934@yandex.ru>
2019-06-14 00:45:10 +0400

    Commit with regexp1

---
+++ Lesson2/Task_add.pl
@@ -0,0 +1,89 @@
+#!/usr/bin/perl
+use strict;
+use Data::Dumper;
+use 5.026;
+
+my $news = [
+			{
+			 new_id => 1,
+			 new_title => '',
+			 position => 1,
+			 slides => [{
+						 slide_id => '',
+						 slide_title => '',
+						 slide_content => '',
+						 position => 1,
+						 },
+						 {
+						 slide_id => '',
+						 slide_title => '',
+						 slide_content => '',
+						 position => 2,
+						 },
+					   ]
+			},
+			
+			{
+			 new_id => 2,
+			 new_title => '',
+			 position => 2,
+			 slides => [{
+						 slide_id => '',
+						 slide_title => '',
+						 slide_content => '',
+						 position => 1,
+						 },
+						 {
+						 slide_id => '',
+						 slide_title => '',
+						 slide_content => '',
+						 position => 2,
+						 },
+						 ]
+			},
+		   ]
+
+my $news = [
+{
+new_id => 1,
+new_title => '',
+position => 1,
+slide_id => '',
+slide_title => '',
+slide_content => '',
+position => 1,
+
+},
+{
+new_id => 1,
+new_title => '',
+position => 1,
+slide_id => '',
+slide_title => '',
+slide_content => '',
+position => 2,
+
+},
+{
+new_id => 2,
+new_title => '',
+position => 2,
+slide_id => '',
+slide_title => '',
+slide_content => '',
+position => 1,
+
+},
+{
+new_id => 2,
+new_title => '',
+position => 2,
+slide_id => '',
+slide_title => '',
+slide_content => '',
+position => 2,
+
+},
+
+]
+
