6c08aabd4ac3f47f9a54845468a1cde44289d2ed
Александр Новожилов <mb934@yandex.ru>
2019-06-20 01:21:50 +0400

    Add task2 solution

---
+++ Lesson7/Tasks/Reg_replace.pl
@@ -0,0 +1,40 @@
+#!/usr/bin/perl
+use strict;
+use 5.026;
+use Data::Dumper;
+
+my @Mod = (
+    'Model X-100 this is Armored Vehicle',
+    'Model X-911 this is Armored Vehicle',
+    'Model A-112 this is Artillery',
+    'Model A-007 this is Artillery',
+    'Model S-777 this is BattleShip',
+    'Model X-014 this is Armored Vehicle',
+    'Model S-212 this is BattleShip',
+    'Model A-555 this is Artillery',
+    'Model K-818 this is Winged Rocket',
+    'Model K-868 this is Winged Rocket',
+);
+
+my %Mod_Alias = (
+    'X-100' => 'Exterminator',
+    'X-911' => 'Extractor',
+    'A-112' => 'Annihilator',
+    'A-007' => 'Devastator',
+    'S-777' => 'Admiral',
+    'X-014' => 'Terminator',
+    'S-212' => 'Godzilla',
+    'A-555' => 'Hellboy',
+    'K-818' => 'Bulava',
+    'K-868' => 'Satana',
+);
+
+foreach (@Mod){
+    $_ =~ s/([A-Z]-\d{3})/$Mod_Alias{$1}/;
+}
+print Dumper @Mod;
+
+# my @Rep_Mod = map {
+#     $_ =~ s/([A-Z]-\d{3})/$Mod_Alias{$1}/;
+# } @Mod;
+# print @Rep_Mod[0];
